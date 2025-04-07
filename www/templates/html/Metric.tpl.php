0e900882338773e0b717974eebc92d58ca835915
Thomas Neumann <thomasneumann21800@gmail.com>
2025-04-07 09:43:39 -0500

    Update WDN link

--- www/templates/html/Metric.tpl.php
+++ www/templates/html/Metric.tpl.php
@@ -1,4 +1,4 @@
 <p>
-    Search Engine Optimization (SEO) is critical to improving traffic to your website. While the most important factor of SEO is the quality of the content on the page (something that can not be automatically analyzed), there are a few things you can do to improve your SEO. <a href="http://wdn.unl.edu/documentation/unl-webaudit/metric-seo">View more documentation on the SEO metric</a>.
+    Search Engine Optimization (SEO) is critical to improving traffic to your website. While the most important factor of SEO is the quality of the content on the page (something that can not be automatically analyzed), there are a few things you can do to improve your SEO. <a href="https://wdn.unl.edu/documentation/web-audit/metric-seo/">View more documentation on the SEO metric</a>.
 </p>
 
